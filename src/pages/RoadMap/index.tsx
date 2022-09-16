29de384f41963e5e1ce426ba3f903b5ae63c9964
Millie <jay.lee.2280@gmail.com>
2022-09-16 23:57:51 +0900

    [#7] Design: implement Roadmap page

---
+++ src/pages/RoadMap/index.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import * as S from './style';
+
+import { Banner } from '@/components/common/Banner';
+import { BANNER, STEPS } from '@/constants/roadmap';
+import useMovePage from '@/hooks/useMovePage';
+import { Container } from '@/styles/common';
+
+// TODO: 카드에 사진 추가하는 디자인으로 변경 시 image URL 추가
+const Step = ({ stepNum, title, contents, imgUrl, link }) => {
+  const [goPage] = useMovePage(link);
+
+  return (
+    <S.StepBox onClick={goPage}>
+      <S.StepLine>
+        <p>Step {stepNum}.</p>
+        <p>{title}</p>
+      </S.StepLine>
+      <S.Card>
+        {/* <img src={imgUrl} alt={title} width="300px" /> */}
+        <p>
+          {contents.map(content => (
+            <span style={{ display: 'block', textAlign: 'center' }}>{content}</span>
+          ))}
+        </p>
+      </S.Card>
+    </S.StepBox>
+  );
+};
+
+export const RoadMap = () => {
+  return (
+    <>
+      <Banner
+        mainText={BANNER.main}
+        subText={BANNER.sub}
+        background="https://www.wallpapertip.com/wmimgs/2-25504_galaxy-wallpaper-dual-screen.jpg"
+        size="large"
+      />
+      <Container>
+        <S.RoadMapBox>
+          {STEPS.map(v => (
+            <Step {...v} />
+          ))}
+        </S.RoadMapBox>
+      </Container>
+    </>
+  );
+};
