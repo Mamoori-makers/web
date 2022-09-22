ca63bd1fe786a569aa0e7e04bd9dc2f18026df1e
Millie <jay.lee.2280@gmail.com>
2022-09-22 23:56:54 +0900

    [#13] Design: implement `My` page

--- src/pages/My/index.tsx
+++ src/pages/My/index.tsx
@@ -1,3 +1,18 @@
+import { Banner } from '@/components/common/Banner';
+import { BANNER_IMG_URL } from '@/constants/bannerImage';
+import { BANNER } from '@/constants/my';
+import { Container } from '@/styles/common';
+
 export const My = () => {
-  return <div>My</div>;
+  return (
+    <>
+      <Banner
+        mainText={BANNER.main}
+        subText={BANNER.sub}
+        background={BANNER_IMG_URL.my}
+        size="large"
+      />
+      <Container>.</Container>
+    </>
+  );
 };
