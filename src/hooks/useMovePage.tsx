6bb6300e83db78f70a1f6448af9578085d75d905
Millie <jay.lee.2280@gmail.com>
2023-02-21 10:50:57 +0900

    Refactor: update .prettierrc

--- src/hooks/useMovePage.tsx
+++ src/hooks/useMovePage.tsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom';
 
 type UseMovePage = (path: string | string[]) => (() => void)[];
 
-export const useMovePage: UseMovePage = path => {
+export const useMovePage: UseMovePage = (path) => {
   const navigate = useNavigate();
 
   if (typeof path === 'string') {
     path = [path];
   }
 
-  return path.map(p => () => navigate(p));
+  return path.map((p) => () => navigate(p));
 };
