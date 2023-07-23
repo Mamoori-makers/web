ddff1cf69beb880e012683baa84a7b35fa9b6440
Jayun <jay.lee.2280@gmail.com>
2023-07-23 17:08:01 +0900

    [#17] Feat: add intro images and constants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import { Banner } from '@/components/Banner';
-import { GNB } from '@/components/GNB';
+import { IntroItem, IntroTextBox } from '@/components/Intro';
 import { BANNER_DATA } from '@/constants/bannerData';
+import { IntroItemData } from '@/constants/introItemData';
 
 export default function Home() {
   return (
-    <main className="">
-      <GNB />
+    <main>
       <Banner bannerData={BANNER_DATA.root} />
+      <IntroTextBox />
+      <div className="mt-5 flex flex-col items-center">
+        {IntroItemData.map((data) => (
+          <IntroItem data={data} key={data.id} />
+        ))}
+      </div>
     </main>
   );
 }
