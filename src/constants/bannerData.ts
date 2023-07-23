19f003d773b31d97d28cf575f1027f1d00383fca
Jayun <jay.lee.2280@gmail.com>
2023-07-23 15:30:13 +0900

    [#17] Feat: implement bannerData

---
+++ src/constants/bannerData.ts
@@ -0,0 +1,43 @@
+export const BANNER_DATA = {
+  root: {
+    imageName: '1.jpg',
+    mainText: '누구나 해야 하는 삶의 마무리.',
+    subText: ['체계적인 죽음 준비, 막막하신가요?', '삶의 매듭을 짓는 일, Mamoori가 도와드릴게요.'],
+  },
+  aboutUs: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+  community: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+  roadmap: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+
+  handbook: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+  will: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+  checklist: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+  myPage: {
+    imageName: '',
+    mainText: '',
+    subText: [],
+  },
+} as const;
