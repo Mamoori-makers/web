684b09e194c254e1d121098e445f212ebeb4fb63
Jayun <jay.lee.2280@gmail.com>
2023-07-23 17:22:51 +0900

    [#17] Refactor: modify useChangeBgByScroll hook

--- src/components/GNB/useChangeBgByScroll.tsx
+++ src/components/GNB/useChangeBgByScroll.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
 
 export const useChangeBgByScroll = () => {
   const GNB_HEIGHT_PX = 64;
   const GNB_BACKGROUND_STYLE = 'linear-gradient(to right, #413C3A, #67615E)';
-  const [GNBBackground, setGNBBackground] = useState(
-    window.scrollY > GNB_HEIGHT_PX ? GNB_BACKGROUND_STYLE : ''
-  );
+  const [GNBBackground, setGNBBackground] = useState('');
 
   const changeGNBBackground = () => {
     window.scrollY > GNB_HEIGHT_PX ? setGNBBackground(GNB_BACKGROUND_STYLE) : setGNBBackground('');
   };
+
+  useLayoutEffect(() => {
+    setGNBBackground(window.scrollY > GNB_HEIGHT_PX ? GNB_BACKGROUND_STYLE : '');
+  }, []);
 
   useEffect(() => {
     window.addEventListener('scroll', changeGNBBackground);
