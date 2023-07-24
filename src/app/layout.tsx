fd1670bfe18e9d32d5abc5c9aa3409ba14015e04
Jayun <jay.lee.2280@gmail.com>
2023-07-24 16:04:13 +0900

    Feat: add openGraph to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Mamoori',
   description: '삶의 마무리를 체계적으로 도와주는 서비스, Mamoori.',
+  openGraph: {
+    title: 'Mamoori',
+    description: '삶의 마무리를 체계적으로 도와주는 서비스, Mamoori.',
+    images: ['/assets/favicon.png'],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
