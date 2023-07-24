8907763112a434fdb7d2a6b80f60c28054256184
Jayun <jay.lee.2280@gmail.com>
2023-07-24 15:34:58 +0900

    Setting: modify theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'brown-100': '#67615E',
+        'brown-200': '#413C3A',
+        'brown-300': '#221a17',
       },
     },
   },
