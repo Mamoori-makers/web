7f3ef469bd11dff2279dc359d8840c46197637ee
Jayun <jay.lee.2280@gmail.com>
2023-07-25 06:34:25 +0900

    Feat: add home link to SquareLogo in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 import { SquareLogo } from '@/components/SquareLogo';
 
 export const Footer = () => {
   return (
     <div className="flex flex-col gap-3 bg-gradient-to-r from-brown-200 to-brown-100 p-7 text-white md:px-14  md:py-6 lg:px-28 lg:py-12">
-      <SquareLogo />
+      <Link href="/">
+        <SquareLogo />
+      </Link>
       <p className="my-2 text-base lg:my-4 lg:text-lg">
         Embrace life&apos;s end with grace - our site helps you prepare.
       </p>
