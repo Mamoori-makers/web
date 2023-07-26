9e77eed6472bc128b1101ccc943c4a3ee309963f
Jayun <jay.lee.2280@gmail.com>
2023-07-26 16:58:44 +0900

    [#27] Design: modify Login Button UI

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function Login() {
             href="#"
             className="mt-4 flex items-center justify-center rounded-3xl border text-gray-600 transition-colors duration-300 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
           >
-            <div className="px-4 py-2">
+            <div className="py-2">
               <svg className="h-6 w-6" viewBox="0 0 40 40">
                 <path
                   d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
@@ -40,13 +40,15 @@ export default function Login() {
                 />
               </svg>
             </div>
-            <span className="w-5/6 px-4 py-3 text-center font-bold">Google로 계속하기</span>
+            <span className="w-5/6 px-4 py-3 text-center text-sm font-semibold">
+              Google로 계속하기
+            </span>
           </Link>
           <Link
             href="#"
             className="mt-4 flex items-center justify-center rounded-3xl border text-gray-600 transition-colors duration-300 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-600"
           >
-            <div className="px-4 py-2">
+            <div className="py-2">
               <svg
                 width="20"
                 height="20"
@@ -71,7 +73,9 @@ export default function Login() {
                 </defs>
               </svg>
             </div>
-            <span className="w-5/6 px-4 py-3 text-center font-bold">Naver로 계속하기</span>
+            <span className="w-5/6 px-4 py-3 text-center text-sm font-semibold">
+              Naver로 계속하기
+            </span>
           </Link>
         </div>
       </div>
