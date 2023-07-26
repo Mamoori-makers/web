7a59e59ea5ccb0f87d00aefd56044f507b70905b
Jayun <jay.lee.2280@gmail.com>
2023-07-26 15:15:25 +0900

    [#25] Refactor: extract roadmap data from roadmap page component

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,31 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { gradientTextStyle } from '@/constants/classNames/gradientTextStyle';
-
-const ROADMAP_CONTENTS = [
-  {
-    title: 'Step 1. Handbook으로 배우기',
-    description:
-      '매일 꺼내볼 수 있는 핸드북에서는 죽음과 관련된 다양한 자료들을 찾아보고, 배워볼 수 있어요.',
-    link: '/roadmap/handbook',
-    image: '/assets/roadmap/handbook.png',
-  },
-  {
-    title: 'Step 2. 유서 작성하기',
-    description:
-      '유서를 작성해 보신 적이 없는 여러분들을 위해, 어떻게 작성해야 하는지 가이드를 준비했어요.',
-    link: '/roadmap/will',
-    image: '/assets/roadmap/last-will.png',
-  },
-  {
-    title: 'Step 3. 체크리스트로 확인하기',
-    description:
-      '죽음에 대해 준비가 어느 정도로 되어 있는지 문항들을 체크해보고, 부족한 부분은 보완해 보아요.',
-    link: '/roadmap/checklist',
-    image: '/assets/roadmap/checklist.png',
-  },
-];
+import { ROADMAP_CONTENTS } from '@/constants/roadmapData';
 
 export default function Roadmap() {
   return (
@@ -35,7 +11,9 @@ export default function Roadmap() {
       </p>
       <div className="mb-3 text-xl font-bold lg:text-2xl">
         <h2>자신감 있는 마무리를 위하여,</h2>
-        <h2 className={gradientTextStyle}>죽음을 대비하는 체계적인 로드맵</h2>
+        <h2 className="bg-gradient-to-r from-yellow-800 to-yellow-600 bg-clip-text text-transparent">
+          죽음을 대비하는 체계적인 로드맵
+        </h2>
       </div>
       <div className="flex flex-col gap-3 lg:flex-row">
         {ROADMAP_CONTENTS.map(({ title, description, link, image }) => {
---
+++ src/constants/roadmapData.ts
@@ -0,0 +1,42 @@
+export const ROADMAP_STEPS = {
+  step1: {
+    id: 'handbook',
+    title: 'Step 1. Handbook으로 배우기',
+    subtitle: 'Embracing Lifelong Learning',
+    image: '/assets/roadmap/handbook.png',
+  },
+  step2: {
+    id: 'will',
+    title: 'Step 2. 유서 작성하기',
+    subtitle: 'Crafting Your Legacy',
+    image: '/assets/roadmap/last-will.png',
+  },
+  step3: {
+    id: 'checklist',
+    title: 'Step 3. 체크리스트로 확인하기',
+    subtitle: 'Assessing Readiness Checklist',
+    image: '/assets/roadmap/checklist.png',
+  },
+};
+
+export const ROADMAP_CONTENTS = [
+  {
+    ...ROADMAP_STEPS.step1,
+    description:
+      '매일 꺼내볼 수 있는 핸드북에서는 죽음과 관련된 다양한 자료들을 찾아보고, 배워볼 수 있어요.',
+    link: '/roadmap/handbook',
+  },
+  {
+    ...ROADMAP_STEPS.step2,
+
+    description:
+      '유서를 작성해 보신 적이 없는 여러분들을 위해, 어떻게 작성해야 하는지 가이드를 준비했어요.',
+    link: '/roadmap/will',
+  },
+  {
+    ...ROADMAP_STEPS.step3,
+    description:
+      '죽음에 대해 준비가 어느 정도로 되어 있는지 문항들을 체크해보고, 부족한 부분은 보완해 보아요.',
+    link: '/roadmap/checklist',
+  },
+];
