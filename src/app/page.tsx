0e63d6de7626840fa37c17ae38392c2d6fa765a4
Jayun <jay.lee.2280@gmail.com>
2023-07-28 13:18:26 +0900

    [#19] Refactor: modify Intro component to Compound Component for reuse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Banner } from '@/components/Banner';
 import { Footer } from '@/components/Footer';
 import { GNB } from '@/components/GNB';
-import { IntroItem, IntroTextBox } from '@/components/Intro';
+import { IntroItem } from '@/components/Intro';
+import { HomeIntro } from '@/components/Intro/HomeIntro';
 import { BANNER_DATA } from '@/constants/bannerData';
 import { IntroItemData } from '@/constants/introItemData';
 
@@ -11,7 +12,7 @@ export default function Home() {
       <GNB />
       <main>
         <Banner bannerData={BANNER_DATA.root} />
-        <IntroTextBox />
+        <HomeIntro />
         <div className="mt-5 flex flex-col items-center">
           {IntroItemData.map((data) => (
             <IntroItem data={data} key={data.id} />
---
+++ src/components/Intro/HomeIntro.tsx
@@ -0,0 +1,31 @@
+import { gradientTextStyle } from '@/constants/classNames/gradientTextStyle';
+
+import { IntroTextBox, IntroTitle, IntroContents } from './IntroTextBox';
+
+export const HomeIntro = () => {
+  return (
+    <IntroTextBox>
+      <IntroTitle>
+        <span className={`font-extrabold ${gradientTextStyle}`}>마무리</span>는 어떤 서비스인가요?
+      </IntroTitle>
+      <IntroContents>
+        <p>
+          일상을 살다 보면 우리는 <span className="block sm:hidden"></span>죽음에 대해서 잊고 지낼
+          때가 많아요.
+        </p>
+        <p>
+          하지만 {''}
+          <span className="font-bold underline decoration-yellow-600 decoration-wavy">
+            죽음은 우리 모두가 마주해야 하기 때문에,
+          </span>
+          <span className="block sm:hidden"></span>
+          이에 대한 대비가 꼭 필요해요.
+        </p>
+        <p>
+          죽음을 어떻게 준비해야 하는지 막막한 분들을 위해 <span className="block sm:hidden"></span>
+          마무리가 준비했어요.
+        </p>
+      </IntroContents>
+    </IntroTextBox>
+  );
+};
