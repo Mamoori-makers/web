0e5f1b09bd491b58534175996036602ff5fd0226
Jayun <jay.lee.2280@gmail.com>
2023-07-29 22:32:46 +0900

    [#29] Design: install @tailwindcss/typography and apply to markdown

--- src/components/Article/Markdown.tsx
+++ src/components/Article/Markdown.tsx
@@ -2,5 +2,9 @@ import ReactMarkdown from 'react-markdown';
 import slug from 'remark-slug';
 
 export const Markdown = ({ markdownString }: { markdownString: string }) => {
-  return <ReactMarkdown remarkPlugins={[slug]}>{markdownString}</ReactMarkdown>;
+  return (
+    <ReactMarkdown className="markdown" remarkPlugins={[slug]}>
+      {markdownString}
+    </ReactMarkdown>
+  );
 };
