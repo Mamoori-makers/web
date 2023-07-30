db446451f35ea0a6757f20de6e15586104286034
Jayun <jay.lee.2280@gmail.com>
2023-07-30 14:42:50 +0900

    [#32] Feat: add react-hot-toast for CopyLinkButton

--- src/components/CopyLinkButton/index.tsx
+++ src/components/CopyLinkButton/index.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 import { LinkIcon } from './LinkIcon';
 
 export const CopyLinkButton = () => {
   const [link, setLink] = useState('');
+
+  const notifyCopied = () => toast('링크가 복사되었습니다.');
 
   useEffect(() => {
     setLink(window.location.href);
@@ -13,18 +16,31 @@ export const CopyLinkButton = () => {
 
   const handleCopyLinkButtonClick = () => {
     navigator.clipboard.writeText(link);
+    notifyCopied();
   };
 
   return (
-    <button
-      className="truncate rounded-md bg-stone-400 p-2 drop-shadow-md"
-      type="button"
-      onClick={handleCopyLinkButtonClick}
-    >
-      <div className="flex items-center justify-center">
-        <LinkIcon />
-        <span className="ml-1 text-sm font-light tracking-widest text-white">Copy Link</span>
-      </div>
-    </button>
+    <>
+      <button
+        className="truncate rounded-md bg-stone-400 p-2 drop-shadow-md"
+        type="button"
+        onClick={handleCopyLinkButtonClick}
+      >
+        <div className="flex items-center justify-center">
+          <LinkIcon />
+          <span className="ml-1 text-sm font-light tracking-widest text-white">Copy Link</span>
+        </div>
+      </button>
+      <Toaster
+        containerStyle={{
+          top: 80,
+        }}
+        toastOptions={{
+          style: {
+            background: 'rgb(214 211 209)',
+          },
+        }}
+      />
+    </>
   );
 };
