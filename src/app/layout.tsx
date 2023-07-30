1efdd559af5c29392863f08ab1dcf5485a387e72
Jayun <jay.lee.2280@gmail.com>
2023-07-30 21:27:41 +0900

    [#33] Feat: apply Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,47 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 import type { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: 'Mamoori',
+const MAMOORI_METADATA = {
+  name: 'Mamoori',
   description: '삶의 마무리를 체계적으로 도와주는 서비스, Mamoori.',
+  mainImage: '/assets/open-graph-image.jpg',
+  author: 'Mamoori Makers',
+  url: 'https://mamoori.life',
+};
+
+export const metadata: Metadata = {
+  title: MAMOORI_METADATA.name,
+  applicationName: MAMOORI_METADATA.name,
+  description: MAMOORI_METADATA.description,
+  authors: [{ name: MAMOORI_METADATA.author }],
   openGraph: {
-    title: 'Mamoori',
-    description: '삶의 마무리를 체계적으로 도와주는 서비스, Mamoori.',
-    images: ['/assets/open-graph-image.jpg'],
+    title: MAMOORI_METADATA.name,
+    description: MAMOORI_METADATA.description,
+    images: [MAMOORI_METADATA.mainImage],
+    url: MAMOORI_METADATA.url,
   },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const gaTrackingId = process.env.GA_TRACKING_ID;
+
   return (
     <html lang="en">
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${gaTrackingId});
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
