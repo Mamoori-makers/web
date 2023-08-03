ba7d7cc34534cd460981f3f717fa78f3465d9691
Jayun <jay.lee.2280@gmail.com>
2023-08-04 08:16:26 +0900

    [#35] Feat: change checklist text to be checked by clicking on it

--- src/app/roadmap/checklist/page.tsx
+++ src/app/roadmap/checklist/page.tsx
@@ -13,9 +13,10 @@ import { CheckIcon } from './checkIcon';
 type CheckItemProps = {
   content: string;
   setCheckCount: Dispatch<SetStateAction<number>>;
+  id: string;
 };
 
-const CheckItem = ({ content, setCheckCount }: CheckItemProps) => {
+const CheckItem = ({ content, setCheckCount, id }: CheckItemProps) => {
   const handleCheck = (isChecked: boolean) => {
     isChecked ? setCheckCount((prev) => prev + 1) : setCheckCount((prev) => prev - 1);
   };
@@ -23,6 +24,7 @@ const CheckItem = ({ content, setCheckCount }: CheckItemProps) => {
   return (
     <div className="flex p-2">
       <Checkbox.Root
+        id={id}
         className="flex h-5 w-5 min-w-[20px] items-center justify-center rounded-md bg-stone-300 shadow-md shadow-stone-500/50 hover:bg-stone-400"
         onCheckedChange={handleCheck}
       >
@@ -30,7 +32,7 @@ const CheckItem = ({ content, setCheckCount }: CheckItemProps) => {
           <CheckIcon />
         </Checkbox.Indicator>
       </Checkbox.Root>
-      <label className="pl-3" htmlFor="c1">
+      <label className="pl-3" htmlFor={id}>
         {content}
       </label>
     </div>
@@ -62,7 +64,14 @@ export default function Checklist() {
       <div className="p-3 text-end text-sm text-brown-200">{`${checkCount} / ${CHECKLIST.length}`}</div>
       <div className="mb-5">
         {CHECKLIST.map(({ id, content }) => {
-          return <CheckItem key={id} content={content} setCheckCount={setCheckCount} />;
+          return (
+            <CheckItem
+              key={id}
+              content={content}
+              setCheckCount={setCheckCount}
+              id={id.toString()}
+            />
+          );
         })}
       </div>
       <div className="flex flex-col items-center">
