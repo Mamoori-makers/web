e0bb2ea4f2b20d664c271eab4fd825652835fc2b
Jayun <jay.lee.2280@gmail.com>
2023-08-06 18:26:54 +0900

    [#34] Feat: add get cookie logic and my page

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,3 +1,37 @@
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
+
+const getCookie = (name: string) => {
+  const cookies = document.cookie.split('; ');
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].split('=');
+    if (cookie[0] === name) {
+      return cookie[1];
+    }
+  }
+  return null;
+};
+
+const ACCESS_TOKEN_COOKIE_KEY = 'Authorization';
+
 export default function Callback() {
+  const setLoginState = useSetAtom(loginStateAtom);
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = getCookie(ACCESS_TOKEN_COOKIE_KEY);
+    if (accessToken?.length) {
+      setLoginState(true);
+      router.push('/my-page');
+    } else {
+      router.push('/');
+    }
+  }, []);
+
   return <div>Loading...</div>;
 }
