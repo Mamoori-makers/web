e4594c0dcde81353c31ddce4db4d7692bd81c6c9
Jayun <jay.lee.2280@gmail.com>
2023-08-08 14:58:01 +0900

    [#37] Design: implement basic my page design

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,3 +1,20 @@
+import { Footer } from '@/components/Footer';
+import { GNB } from '@/components/GNB';
+import { ProfileImage } from '@/components/ProfileImage';
+
 export default function MyPage() {
-  return <div className="">마이 페이지</div>;
+  return (
+    <>
+      <GNB />
+      <main className="flex flex-col items-center">
+        <div className="flex h-[400px] w-full justify-center bg-brown-100">
+          <div className="flex flex-col items-center justify-center">
+            <ProfileImage imageSrc="/assets/mamoori_profile.png" alt="profile" size={100} />
+            <p className="my-3 text-white">Email</p>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
