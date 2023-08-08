dbb7dc7d122a044934ce537656470ec43db9099b
Jayun <jay.lee.2280@gmail.com>
2023-08-08 14:44:16 +0900

    [#34] Feat: add set Access token login in Callback component

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -4,22 +4,26 @@ import { useSetAtom } from 'jotai';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
 import { getCookie } from '@/utils/cookie';
 
 const ACCESS_TOKEN_COOKIE_KEY = 'Authorization';
 
 export default function Callback() {
+  const setAccessToken = useSetAtom(accessTokenAtom);
   const setLoginState = useSetAtom(loginStateAtom);
   const router = useRouter();
 
   useEffect(() => {
     const accessToken = getCookie(ACCESS_TOKEN_COOKIE_KEY);
     if (accessToken?.length) {
+      setAccessToken(accessToken);
       setLoginState(true);
       router.push('/my-page');
     } else {
       router.push('/');
+      alert('로그인에 실패하였습니다. 다시 시도해 주세요.');
     }
   }, []);
 
