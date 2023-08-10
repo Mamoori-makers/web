d8bfd3c16ca445636de1bca1a67be3370157ad13
Jayun <jay.lee.2280@gmail.com>
2023-08-10 19:38:06 +0900

    [#37] Refactor: move UserData type to useUserData query and add return type

--- src/libs/react-query/useUserData.ts
+++ src/libs/react-query/useUserData.ts
@@ -1,11 +1,18 @@
-import { useQuery } from '@tanstack/react-query';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
 
 import { API_PATH } from '@/constants/apiPath';
 import { getAuthRequest } from '@/libs/axios/useAuthApi';
 
 import { QUERY_KEY } from './queryKey';
 
-export const useUserData = (accessToken: string) => {
+export type UserData = {
+  userId: number;
+  email: string;
+  name: string;
+  image: string;
+};
+
+export const useUserData = (accessToken: string): UseQueryResult<UserData> => {
   return useQuery({
     queryKey: [QUERY_KEY.user, accessToken],
     queryFn: () => getAuthRequest(API_PATH.user, accessToken),
--- src/stores/atoms/userDataAtom.ts
+++ src/stores/atoms/userDataAtom.ts
@@ -1,14 +1,5 @@
 import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 
-const storage = createJSONStorage(() => {
-  return sessionStorage;
-});
-
-export type UserData = {
-  userId: number;
-  email: string;
-  name: string;
-  image: string;
-};
+const storage = createJSONStorage(() => sessionStorage);
 
 export const userDataAtom = atomWithStorage('userData', null, storage);
