1f46f2f42af079852b5687a444e9dc805ac3c0e0
Jayun <jay.lee.2280@gmail.com>
2023-08-11 08:51:15 +0900

    [#37] Refactor: modify callback component logic

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAtom, useSetAtom } from 'jotai';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { useUserData } from '@/libs/react-query/useUserData';
@@ -16,23 +16,26 @@ export default function Callback() {
   const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
   const setLoginState = useSetAtom(loginStateAtom);
   const setUserData = useSetAtom(userDataAtom);
+  const router = useRouter();
   const { data: userData, isSuccess } = useUserData(accessToken);
 
   useEffect(() => {
-    const accessToken = getCookie(ACCESS_TOKEN_COOKIE_KEY);
-    if (accessToken?.length) {
-      setAccessToken(accessToken);
+    const accessTokenInCookie = getCookie(ACCESS_TOKEN_COOKIE_KEY);
+    if (accessTokenInCookie?.length) {
+      setAccessToken(accessTokenInCookie);
+      setLoginState(true);
     }
 
     if (isSuccess) {
       setUserData(userData);
-      setLoginState(true);
-      redirect('/my-page');
-    } else {
-      alert('로그인에 실패했습니다. 다시 시도해 주세요.');
-      redirect('/login');
+      router.push('/my-page');
+      return;
     }
-  }, [isSuccess, userData, accessToken, setAccessToken, setLoginState, setUserData]);
+
+    if (!userData) {
+      router.push('/login');
+    }
+  }, [isSuccess, setAccessToken, userData, setUserData, setLoginState, accessToken, router]);
 
   return <></>;
 }
