87c23cc6ad15078c46087849fde8346811d73c94
Jayun <jay.lee.2280@gmail.com>
2023-08-10 20:31:59 +0900

    [#37] Refactor: move setUserData logic to Callback component

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,31 +1,38 @@
 'use client';
 
-import { useSetAtom } from 'jotai';
-import { useRouter } from 'next/navigation';
+import { useAtom, useSetAtom } from 'jotai';
+import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
 
+import { useUserData } from '@/libs/react-query/useUserData';
 import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
+import { userDataAtom } from '@/stores/atoms/userDataAtom';
 import { getCookie } from '@/utils/cookie';
 
 const ACCESS_TOKEN_COOKIE_KEY = 'Authorization';
 
 export default function Callback() {
-  const setAccessToken = useSetAtom(accessTokenAtom);
+  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
   const setLoginState = useSetAtom(loginStateAtom);
-  const router = useRouter();
+  const setUserData = useSetAtom(userDataAtom);
+  const { data: userData, isSuccess } = useUserData(accessToken);
 
   useEffect(() => {
     const accessToken = getCookie(ACCESS_TOKEN_COOKIE_KEY);
     if (accessToken?.length) {
       setAccessToken(accessToken);
-      setLoginState(true);
-      router.push('/my-page');
-    } else {
-      router.push('/');
-      alert('로그인에 실패하였습니다. 다시 시도해 주세요.');
     }
-  }, []);
+
+    if (isSuccess) {
+      setUserData(userData);
+      setLoginState(true);
+      redirect('/my-page');
+    } else {
+      alert('로그인에 실패했습니다. 다시 시도해 주세요.');
+      redirect('/login');
+    }
+  }, [isSuccess, userData, accessToken, setAccessToken, setLoginState, setUserData]);
 
   return <></>;
 }
--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,58 +1,54 @@
 'use client';
 
-import { useAtomValue, useSetAtom, useAtom } from 'jotai';
+import { useAtomValue } from 'jotai';
 import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 import { Footer } from '@/components/Footer';
 import { GNB } from '@/components/GNB';
 import { ProfileImage } from '@/components/ProfileImage';
-import { useUserData } from '@/libs/react-query/useUserData';
-import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
+import { UserData } from '@/libs/react-query/useUserData';
 import { loginStateAtom } from '@/stores/atoms/loginStateAtom';
 import { userDataAtom } from '@/stores/atoms/userDataAtom';
 
 export default function MyPage() {
-  const [isLoggedIn, setIsLoggedIn] = useAtom(loginStateAtom);
-  const setUserData = useSetAtom(userDataAtom);
-  const accessToken = useAtomValue(accessTokenAtom);
+  const isLoggedIn = useAtomValue(loginStateAtom);
+  const userData = useAtomValue(userDataAtom) as UserData | null;
 
   if (!isLoggedIn) {
     redirect('/login');
   }
 
-  const { data: userData, isSuccess } = useUserData(accessToken);
-
-  if (isSuccess && userData) {
-    setUserData(userData);
-    setIsLoggedIn(true);
-
-    const { image, name, email } = userData;
-
-    return (
-      <>
-        <GNB />
-        <main className="flex flex-col items-center">
-          <div className="relative flex h-[400px] w-full items-center justify-center bg-brown-200">
-            <Image
-              src="/assets/banner/my.jpeg"
-              fill
-              priority
-              alt="background"
-              style={{ objectFit: 'cover' }}
-            />
-            <div
-              className="flex h-fit w-[300px] flex-col items-center justify-center gap-2 rounded-lg bg-[#473d3d5c] p-5 text-white"
-              style={{ zIndex: 1 }}
-            >
-              <ProfileImage imageSrc={image} alt="profile" size={100} />
-              <p className="font-bold">{name}</p>
-              <p className="text-sm">{email}</p>
-            </div>
-          </div>
-        </main>
-        <Footer />
-      </>
-    );
+  if (!userData) {
+    alert('데이터를 불러오지 못했습니다. 다시 로그인 해 주세요.');
+    redirect('/login');
   }
+
+  const { image, name, email } = userData;
+
+  return (
+    <>
+      <GNB />
+      <main className="flex flex-col items-center">
+        <div className="relative flex h-[400px] w-full items-center justify-center bg-brown-200">
+          <Image
+            src="/assets/banner/my.jpeg"
+            fill
+            priority
+            alt="background"
+            style={{ objectFit: 'cover' }}
+          />
+          <div
+            className="flex h-fit w-[300px] flex-col items-center justify-center gap-2 rounded-lg bg-[#473d3d5c] p-5 text-white"
+            style={{ zIndex: 1 }}
+          >
+            <ProfileImage imageSrc={image} alt="profile" size={100} />
+            <p className="font-bold">{name}</p>
+            <p className="text-sm">{email}</p>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
