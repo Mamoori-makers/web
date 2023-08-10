74ac707b3aa8503ef198b20d8238fe96c6d5e9c3
Jayun <jay.lee.2280@gmail.com>
2023-08-10 15:57:04 +0900

    [#37] Feat: add google user content hostname in next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'www.notion.so',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
