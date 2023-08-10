058b898862dbcd0e14bd70d1641a3ab216157565
Jayun <jay.lee.2280@gmail.com>
2023-08-10 14:38:23 +0900

    [#37] Refactor: add accessToken login into useUserData
    accessToken을 얻어오는 로직을 useUserData react query hook으로 옮김

--- src/libs/react-query/useUserData.ts
+++ src/libs/react-query/useUserData.ts
@@ -1,11 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
+import { useAtomValue } from 'jotai';
 
 import { API_PATH } from '@/constants/apiPath';
 import { getAuthRequest } from '@/libs/axios/useAuthApi';
+import { accessTokenAtom } from '@/stores/atoms/accessTokenAtom';
 
 import { QUERY_KEY } from './queryKey';
 
-export const useUserData = (accessToken: string) => {
+export const useUserData = () => {
+  const accessToken = useAtomValue(accessTokenAtom);
+
   return useQuery({
     queryKey: [QUERY_KEY.user, accessToken],
     queryFn: () => getAuthRequest(API_PATH.user, accessToken),
