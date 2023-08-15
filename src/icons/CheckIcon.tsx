92ca201938d029a0b5cf12b73597a5b84ee3d59a
Jayun <jay.lee.2280@gmail.com>
2023-08-15 12:13:07 +0900

    [#40] Refactor: modify strokeColor in CheckIcon

--- src/icons/CheckIcon.tsx
+++ src/icons/CheckIcon.tsx
@@ -1,9 +1,9 @@
-export const CheckIcon = () => {
+export const CheckIcon = ({ strokeColor = 'black' }) => {
   return (
     <svg fill="none" height="20" viewBox="0 0 20 20" width="20">
       <path
         d="M14 7L8.5 12.5L6 10"
-        stroke="black"
+        stroke={strokeColor}
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="2"
